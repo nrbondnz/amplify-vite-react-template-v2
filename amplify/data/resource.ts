c604e71e3bd9d096fba825c875c8686a68851222
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-11-11 23:22:08 +1300

    Update data schema and add error handling in App component
    Replaced the client-side authorization rule from guest to authenticated in `resource.ts`, and changed the schema combination. Enhanced the App component with loading state management, error handling, and dynamic updates to improve user experience.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,26 +1,69 @@
-// amplify/data/resource.ts
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+"use client";
+import {  defineData } from "@aws-amplify/backend";
 import { schema as generatedSqlSchema } from './schema.sql';
 
+// Apply global authorization rule to allow all authenticated users
 const sqlSchema = generatedSqlSchema.authorization(allow => allow.authenticated());
 
-const testSchema = a.schema({
+// Add relationships to the globally authorized schema
+//const sqlSchemaWithRelationships = sqlSchema;
+/*models => [
+    models.machines.relationships({
+       
+        location: a.hasOne("locations", "idLocation")
+    }),
+    models.locations.relationships({
+        machines: a.hasMany("machines", "idLocation")  // Corresponding id to idLocation
+    })
+]*/
+
+
+// Additional test schema
+/*const testSchema = a.schema({
     Todo: a
         .model({
             content: a.string(),
-            isDone: a.boolean(),
+            isDone: a.boolean()
         })
-        .authorization(allow => allow.authenticated()),
-});
+        .authorization(allow => allow.authenticated())
+});*/
 
-const combinedSchema = a.combine([sqlSchema, testSchema]);
+// Combine all schemas
+//const combinedSchema = a.combine([sqlSchema, testSchema]);
 
-console.log("Combined schema: ", combinedSchema);
+console.log("Combined schema: ", sqlSchema);
 
-export type Schema = ClientSchema<typeof combinedSchema>;
+//export type Schema = ClientSchema<typeof combinedSchema>;
 
 export const data = defineData({
-    schema: combinedSchema,
+    schema: sqlSchema,
     authorizationModes: {
+        defaultAuthorizationMode: "apiKey", // Default mode
+        apiKeyAuthorizationMode: {
+            expiresInDays: 30,
+        }
     },
 });
+
+/*
+"use client"
+import { generateClient } from "aws-amplify/data";
+import type { Schema from "@/amplify/data/resource";
+
+const client = generateClient<Schema>(); // use this Data client for CRUDL requests
+*/
+
+/*
+Fetch records from the database and use them in your frontend component.
+For example, in a React component, you can use this snippet in your function's RETURN statement:
+
+const { data: todos } = await client.models.Todo.list();
+
+return (
+  <ul>
+    {todos.map(todo => (
+      <li key={todo.id}>{todo.content}</li>
+    ))}
+  </ul>
+);
+*/
