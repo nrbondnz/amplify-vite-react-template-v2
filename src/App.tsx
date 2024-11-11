53fa858243832218545ae34bd99e2d00921cfdb6
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-11-11 23:00:53 +1300

    Update data schema and add error handling in App component
    Replaced the client-side authorization rule from guest to authenticated in `resource.ts`, and changed the schema combination. Enhanced the App component with loading state management, error handling, and dynamic updates to improve user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
+//import { auth } from "./../amplify/auth/resource";
+import { Amplify } from "aws-amplify";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+
+import { Subscription } from "rxjs";
+import outputs from "../amplify_outputs.json";
+Amplify.configure(outputs);
+
 
 const client = generateClient<Schema>();
 
 function App() {
     const [locations, setLocations] = useState<Array<Schema["locations"]["type"]>>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<Error | null>(null);
+    if (!loading) {
+        fetchLocations()
+        setLoading(true);
+    }
 
+    async function fetchLocations() {
+        try {
+            // Ensure the user is authenticated before making requests
+            //const user: CognitoUser = await
+            // auth.currentAuthenticatedUser();
+            //console.log("App user: ", user)
+            const initialLocations = await client.models.locations.list();
+            setLocations(initialLocations.data);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err);
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+    
     useEffect(() => {
-        const subscription = client.models.locations.observeQuery().subscribe({
-            next: (data) => setLocations([...data.items]),
-        });
+      
 
-        return () => subscription.unsubscribe();
+        fetchLocations();
     }, []);
 
-    function createLocation() {
-        const locationName = window.prompt("Location name");
-        if (locationName) {
-            client.models.locations.create({ entityName: locationName, id: 74 });
+    useEffect(() => {
+        let subscription: Subscription;
+        async function setupSubscription() {
+            try {
+                // Ensure the user is authenticated before setting up the subscription
+                //const user: CognitoUser = await
+                // Auth.currentAuthenticatedUser();
+
+                subscription = client.models.locations.observeQuery().subscribe({
+                    next: (data) => setLocations([...data.items]),
+                });
+            } catch (err) {
+                if (err instanceof Error) {
+                    setError(err);
+                }
+            }
         }
+
+        setupSubscription();
+
+        return () => {
+            if (subscription) {
+                subscription.unsubscribe();
+            }
+        };
+    }, []);
+
+    function createLocation(): void {
+        const locationName: string | null = window.prompt("Location name");
+        if (locationName) {
+            client.models.locations.create({ entityName: locationName, id: Math.random() * 45 * Math.random() });
+        }
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
     }
 
     return (
